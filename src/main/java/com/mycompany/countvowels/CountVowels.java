fc730b9391be56310dd94009e9c019007e3964a1
RC_Student_lab <RC_Student_lab@10.0.0.143>
2024-10-04 14:14:49 +0000

    First commit

---
+++ src/main/java/com/mycompany/countvowels/CountVowels.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.countvowels;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class CountVowels {
+
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        
+        // Ask the user to input text
+        System.out.print("Enter a sentence: ");
+        String sentence = scanner.nextLine();
+        
+        // Count the vowels, and display the results
+        int vowelCount = countVowels(sentence);
+        System.out.println("Number of vowels in a string: " + vowelCount);
+        
+    
+    }
+}
